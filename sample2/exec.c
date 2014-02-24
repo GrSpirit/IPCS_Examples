858ed9062d240fe0de1239efc3b0c3773cb5dbac
Your Name <you@example.com>
2014-02-24 16:32:33 +0400

    Added exec sample

---
+++ sample2/exec.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+	int pid;
+	if (pid = fork()) {
+		wait(0);
+	}
+	else {
+		execlp("ls", "ls", "-l", "-a", "/usr/", NULL);
+	}
+	
+	return 0;
+}
