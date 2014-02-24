188b062fec673ed165f19d199c6b8a22a74491a9
Vitaliy Shcherbakov <gr.spirit@gmail.com>
2014-02-24 22:36:05 +0400

    Modified queue sample to use multiprocess

--- sample4/queue.c
+++ sample4/queue.c
@@ -13,7 +13,7 @@ typedef struct
 
 int main()
 {
-  mymsgbuf msg1, msg2;
+  mymsgbuf msg;
   
   int qid = msgget(IPC_PRIVATE, 0666 | IPC_CREAT | IPC_EXCL);
   if (qid < 0) {
@@ -21,25 +21,32 @@ int main()
     exit(-1);
   }
 
-  msg1.mtype = 1;                               // Формирование сообщения
-  sprintf(msg1.mtext, "Hello queue!");
-  
-  if (msgsnd(qid, &msg1, sizeof(mymsgbuf) - sizeof(long), 0) < 0) {        // Отправка
-    perror("Couldnot send message");
-    exit(-1);
+  if (fork()) {           // Ответвление потомка
+    msg.mtype = 1;                               // Формирование сообщения
+    sprintf(msg.mtext, "Hello queue!");
+    printf("Sending message: %s\n", msg.mtext);
+    
+    if (msgsnd(qid, &msg, sizeof(mymsgbuf) - sizeof(long), 0) < 0) {        // Отправка сообщения
+      perror("Couldnot send message");
+      exit(-1);
+    }
+
+    wait(0);
+
+    if (msgctl(qid, IPC_RMID, NULL) < 0) {        // Освобождение очереди
+      perror("Couldnot free queue");
+      exit(-1);
+    }
   }
+  /* if (fork()) */
+  else {                  // Потомок
+    if (msgrcv(qid, &msg, sizeof(mymsgbuf) - sizeof(long), 0, 0) < 0) {    // Получение сообщения
+      perror("Couldnot receive message");
+      exit(-1);
+    }
 
-  if (msgrcv(qid, &msg2, sizeof(mymsgbuf) - sizeof(long), 0, 0) < 0) {    // Получение
-    perror("Couldnot send message");
-    exit(-1);
+    printf("Received message: %s\n", msg.mtext);
   }
-
-  printf("Message: %s\n", msg2.mtext);        
   
-  if (msgctl(qid, IPC_RMID, NULL) < 0) {        // Освобождение очереди
-    perror("Couldnot free queue");
-    exit(-1);
-  }        // Освобождение очереди
-
   return 0;
 }
